d60e65150e23580e547f66207038fa5661773e12
Saket Kolluru <saketkolluru.27@gmail.com>
2025-02-23 18:56:25 -0500

    debugged file uploads and fixing grammar

--- uploads/main.py
+++ uploads/main.py
@@ -12,22 +12,28 @@ def main():
 @app.route('/success', methods = ['POST'])
 def sucess():
     if request.method == 'POST':
-        f = request.files['file']
+        print(request.files)
+        f = request.files.get('curr')
         name = f.filename
-        f.save(f.filename, app.config['documents'])
-        return render_template('acknowledgement.html', name)
+        file_path = os.path.join(app.config['documents'], name)
+        f.save(file_path)
+        return render_template('acknowledgement.html', n=name)
 
 @app.route('/grammar')
-def grammer(filename):
+def grammer():
     tool = language_tool_python.LanguageTool('en-US')
-    file = open('/' + filename)
-    lines = file.readlines
+    file = request.files.get('curr')
+    name = file.filename
+    file_path = os.path.join(app.config['documents'], name)
+    file.save(file_path)
+    with open(file_path, 'r') as c:
+        lines = c.readlines()
     corr = []
     sugs = []
     for line in lines:
         sugs.append(tool.check(line))
         corr.append(tool.correct(line))
-    return render_template('suggestions.html', {corr, sugs})
+    return render_template('suggestions.html', corr=corr, sugs=sugs)
 
 if __name__ == '__main__':
     app.run(debug=True)
