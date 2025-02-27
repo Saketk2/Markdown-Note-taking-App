8060bf50dd8e040c987e0143e4cc9ccb9fdad8c6
Saket Kolluru <saketkolluru.27@gmail.com>
2025-02-26 22:54:55 -0500

    Finished EP1, working through EP2 and EP3

--- uploads/main.py
+++ uploads/main.py
@@ -1,25 +1,27 @@
 import logging
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 import os
 import language_tool_python
+from werkzeug.utils import secure_filename
+
 app = Flask(__name__)
 app.config['documents'] = 'uploads/documents'
+app.config['database'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class File(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.string(200), nullable= False)
+    data = db.Column(db.LargeBinary, nullable=False)
+
 
 @app.route('/')
 def main():
     return render_template('index.html')
 
-@app.route('/success', methods = ['POST'])
-def sucess():
-    if request.method == 'POST':
-        print(request.files)
-        f = request.files.get('curr')
-        name = f.filename
-        file_path = os.path.join(app.config['documents'], name)
-        f.save(file_path)
-        return render_template('acknowledgement.html', n=name)
-
-@app.route('/grammar')
+@app.route('/grammar', methods = ['POST'])
 def grammer():
     tool = language_tool_python.LanguageTool('en-US')
     file = request.files.get('curr')
@@ -33,7 +35,19 @@ def grammer():
     for line in lines:
         sugs.append(tool.check(line))
         corr.append(tool.correct(line))
-    return render_template('suggestions.html', corr=corr, sugs=sugs)
+    return render_template('suggestions.html', corr=corr, sugs=sugs, name=name)
+
+@app.route('/save', methods = ['POST'])
+def save():
+    file = request.file['curr']
+    if file:
+        filename = secure_filename(file.name)
+        data = file.read()
+        duplicate = File(name=filename, data=data)
+        db.session.add(duplicate)
+        db.session.commit()
+        return "Sucess!"
+    return "Something went wrong:("
 
 if __name__ == '__main__':
     app.run(debug=True)
